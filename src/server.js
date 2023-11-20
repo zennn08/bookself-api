eb1938d7c0f1cf42e2567d36ba9e86f6d5d5f87b
aqulzz <am.fadwa15@gmail.com>
2023-11-20 15:31:08 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const Hapi = require("@hapi/hapi")
+const routes = require("./routes")
+
+const startServer = async () => {
+  const server = Hapi.server({
+    port: 9000,
+    host: "localhost"
+  })
+  server.route(routes)
+  await server.start()
+  console.log(`Server berjalan pada ${server.info.uri}`)
+}
+
+startServer()
